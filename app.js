87647d2e3f7af57ba85cfdaf4bea1ae6c8f4429c
Aditya Kamble <adityakamble49@gmail.com>
2020-12-02 14:48:06 -0500

    CORS Updated

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+
 const DBManager = require('./dbutils/DBManager');
 
 const index = require('./controller/index.js');
 const userController = require('./controller/UserController.js');
 const meetupGroupController = require('./controller/MeetupGroupController.js');
 const eventController = require('./controller/EventController.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.all("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 app.use('/', index);
 app.use('/user', userController);
 app.use('/group', meetupGroupController);
